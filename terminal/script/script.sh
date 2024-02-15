f6ed48052dcb47591e741b85cd8a0da749618229
Federico E. Benelli <fedebenelli@outlook.com>
2024-02-15 09:26:46 -0300

    script detail

--- terminal/script/script.sh
+++ terminal/script/script.sh
@@ -2,12 +2,9 @@
 # ~/.local/bin/fortran_project.sh
 
 nombre=$1  # Primer argumento
-
 # Crea una carpeta 
 fpm new "$nombre"
-
 # Ir a la nueva carpeta
 cd "$nombre"
-
 # Añade configuraciones generales de vscode al proyecto
 git submodule add "git@github.com:ipqa-research/vscode-fortran.git" .vscode
